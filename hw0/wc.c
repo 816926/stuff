62be18e414c266ce2225b1fba2fdd207d52158d3
816926 <816926@students.wits.ac.za>
2016-07-19 11:23:23 +0200

    Finished coding wc.c

--- hw0/wc.c
+++ hw0/wc.c
@@ -7,10 +7,39 @@ void wc ( FILE *infile, char *inname);
 
 int
 main (int argc, char *argv[]) {
+
+if (argc < 1)
+{
+
+} 
+
+
     
 }
 
 void
 wc( FILE *infile, char *inname) {
+       
+infile = fopen (inname, "r");
+
+int words = 0;
+int lines = 0;
+int chars = 0;
+int c;
+bool isspace = true;
+
+	if(c = fgetc(infile) != EOF)
+	{		
+		if(isspace=false)
+		{
+			words++
+		}
+
+		if( c = " " || c = "\n")
+		{	
+			lines++;
+		}	
+
+	}
     
 }
